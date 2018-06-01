6cd1d8b4278ae755d466bf092f32fc969891f5b4
ID - Sergio Puche <spuche@fermax.lan>
2018-06-01 14:46:39 +0200

    Mejoras en los test unitarios

--- src/test/java/com/fermax/spuche/java/FermatTheoremTest.java
+++ src/test/java/com/fermax/spuche/java/FermatTheoremTest.java
@@ -74,10 +74,49 @@ public class FermatTheoremTest
 		assertFalse(FermatTheorem.isPrime(4, 10));
 	}
 	
+	
+	/**
+	 * Tests para el método selectA
+	 */
 	@Test
 	public void selectATest1()
 	{
 		assertTrue(1<FermatTheorem.selectA(10) && FermatTheorem.selectA(10)<10);
 	}
+	
+	/**
+	 * Tests para el método specialCases
+	 */
+	@Test
+	public void specialCasesTest1()
+	{
+		assertEquals(0, FermatTheorem.specialCases(7));
+	}
+	
+	@Test
+	public void specialCasesTest2()
+	{
+		assertEquals(0, FermatTheorem.specialCases(622));
+	}
+	
+	@Test
+	public void specialCasesTest3()
+	{
+		assertEquals(1, FermatTheorem.specialCases(-55));
+	}
+	
+	@Test
+	public void specialCasesTest4()
+	{
+		assertEquals(2, FermatTheorem.specialCases(0));
+	}
+	
+	@Test
+	public void specialCasesTest5()
+	{
+		assertEquals(0, FermatTheorem.specialCases(1));
+	}
+	
+	
 	
 }
