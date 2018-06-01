2bf9eadd69c05f45c67e82d333b530827d82f331
ID - Sergio Puche <spuche@fermax.lan>
2018-06-01 12:35:19 +0200

    Se añade la funcionalidad del programa

--- src/main/java/com/fermax/spuche/java/FermatTheorem.java
+++ src/main/java/com/fermax/spuche/java/FermatTheorem.java
@@ -1,6 +1,46 @@
 package com.fermax.spuche.java;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class FermatTheorem
 {
+	
+	//Comprueba si un número a es divisible por b
+	public static boolean isDivisible(int a, int b)
+	{
+		if(a%b == 0) {return true;}
+		else {return false;}
+	}
+	
+	//Comprueba si el número p es primo siguiendo el Pequeño Teorema de Fermat
+	public static boolean isPrime(int a, int p)
+	{
+		BigInteger bigA = BigInteger.valueOf(a);
+		
+		BigInteger bigRes = bigA.modPow(BigInteger.valueOf(p-1), BigInteger.valueOf(p));
+		int res = bigRes.intValue();
+		
+		if(res==1) {return true;}
+		else {return false;}
+	}
+	
+
+
+	public static void main(String args[])
+	{
+		Scanner keyb = new Scanner(System.in);
+		int a, p;
+		
+		System.out.println("Escriba el número cuya primalidad quiere comprobar:\n");
+		p = keyb.nextInt();
+		
+		a = ThreadLocalRandom.current().nextInt(2, p-1);
+		
+		
+		
+		
+	}
 
 }
