74ea8ad60cba4ad3a27af82a9824c68f39fc81d1
ID - Sergio Puche <spuche@fermax.lan>
2018-06-01 12:58:29 +0200

    Bugfix para que los números 2 y 3 den resultado correcto

--- src/main/java/com/fermax/spuche/java/FermatTheorem.java
+++ src/main/java/com/fermax/spuche/java/FermatTheorem.java
@@ -6,39 +6,77 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class FermatTheorem
 {
-	
-	//Comprueba si un número a es divisible por b
+
+	// Comprueba si un número a es divisible por b
 	public static boolean isDivisible(int a, int b)
 	{
-		if(a%b == 0) {return true;}
-		else {return false;}
+		if (a % b == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
-	
-	//Comprueba si el número p es primo siguiendo el Pequeño Teorema de Fermat
+
+	// Comprueba si el número p es primo siguiendo el Pequeño Teorema de Fermat
 	public static boolean isPrime(int a, int p)
 	{
 		BigInteger bigA = BigInteger.valueOf(a);
-		
-		BigInteger bigRes = bigA.modPow(BigInteger.valueOf(p-1), BigInteger.valueOf(p));
+
+		BigInteger bigRes = bigA.modPow(BigInteger.valueOf(p - 1), BigInteger.valueOf(p));
 		int res = bigRes.intValue();
-		
-		if(res==1) {return true;}
-		else {return false;}
+
+		if (res == 1)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
-
+	public static int selectA(int p)
+	{
+		int a;
+		
+		if(p<4)
+		{
+			do
+			{
+				a = ThreadLocalRandom.current().nextInt(2,20);
+			}
+			while(isDivisible(a,p));
+		}
+		else
+		{
+			a = ThreadLocalRandom.current().nextInt(2, p - 1);
+		}
+		
+		return a;
+	}
 
 	public static void main(String args[])
 	{
 		Scanner keyb = new Scanner(System.in);
 		int a, p;
-		
-		System.out.println("Escriba el número cuya primalidad quiere comprobar:\n");
+
+		System.out.println("Escriba el numero cuya primalidad quiere comprobar:\n");
 		p = keyb.nextInt();
+		keyb.close();
 		
-		a = ThreadLocalRandom.current().nextInt(2, p-1);
+		a = selectA(p);
 		
-		
+		if (isPrime(a, p))
+		{
+			System.out.println("El numero " + p + " es primo");
+		}
+		else
+		{
+			System.out.println("Has fracasado como el perro que eres!");
+		}
 		
 		
 	}
